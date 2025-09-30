b117b30c2881bd5a76d565417ecc73fa1ce9c990
antigx <tiago.mamedio.barbosa@gmail.com>
2025-09-29 21:50:14 -0300

    Definindo tipagem apra produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import Image from "next/image";
+import { ProductType } from "./types/ProductType";
 
-export default function Home() {
+async function fetchProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await fetchProducts();
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap=6">
-        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
-        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
-        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
-        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
+        {products.map((product: ProductType) => (
+          <div key={product.id} className="bg-white p-4 rounded-lg shadow">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-48 object-contain mb-4"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/app/types/ProductType.ts
@@ -0,0 +1,9 @@
+export type ProductType = {
+  id: number;
+  title: string;
+  price: number | null;
+  description: string | null;
+  category: string;
+  image: string;
+  rating: { rate: number; count: number };
+};
