f89339e1a296be2cbbd368c66f18b819a404a868
antigx <tiago.mamedio.barbosa@gmail.com>
2025-09-29 21:41:03 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-Commerce</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap=6">
+        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
+        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
+        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
+        <div className="bg-white rounded-lg p-4 flex flex-col">PROD1</div>
+      </div>
     </div>
   );
 }
