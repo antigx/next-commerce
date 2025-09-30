031543c0bb3251649fc2b21a8e618a3256c5f9e6
antigx <tiago.mamedio.barbosa@gmail.com>
2025-09-29 22:15:28 -0300

    Estilizando componente produto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import clsx from "clsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={clsx(
+          geistSans.variable,
+          geistMono.variable,
+          "antialiased",
+          "bg-slate-700"
+        )}
       >
         <NavBar />
-        <main className="bg-slate-700 h-screen p-16">{children}</main>
+        <main className=" h-screen p-16">{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Product from "./components/Product";
 import { ProductType } from "./types/ProductType";
 
 async function fetchProducts() {
@@ -14,13 +15,7 @@ export default async function Home() {
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap=6">
         {products.map((product: ProductType) => (
-          <div key={product.id} className="bg-white p-4 rounded-lg shadow">
-            <img
-              src={product.image}
-              alt={product.title}
-              className="w-full h-48 object-contain mb-4"
-            />
-          </div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
