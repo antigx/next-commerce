dc0749acf3b1f6fd5e6c7f2983caacc4174965b2
antigx <tiago.mamedio.barbosa@gmail.com>
2025-09-30 17:02:58 -0300

    Configurando prisma e vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/NavBar";
+
+import { ptBR } from "@clerk/localizations";
 import clsx from "clsx";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,18 +34,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={clsx(
-          geistSans.variable,
-          geistMono.variable,
-          "antialiased",
-          "bg-slate-700"
-        )}
-      >
-        <NavBar />
-        <main className=" h-screen p-16">{children}</main>
-      </body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
+        <body
+          className={clsx(
+            geistSans.variable,
+            geistMono.variable,
+            "antialiased",
+            "bg-slate-700"
+          )}
+        >
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton mode="modal" />
+              <SignUpButton mode="modal">
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main className=" h-screen p-16">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
